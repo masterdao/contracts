8467186b4f223c5fd781ffea51e0d7bcfe72b9de
BLV <84831970+cyxywfh@users.noreply.github.com>
2022-06-02 17:22:04 +0800

    Update idovote.sol
    update

--- src/IDO/idovote.sol
+++ src/IDO/idovote.sol
@@ -118,6 +118,15 @@ contract idovoteContract is  Ownable {
         require(who != address(0));
         return voet_p_weight[who].weight;
     }
+    function  getVotePeoperInfo(address who) public view returns(vcoinInfo[] memory){
+        require(who != address(0));
+        uint256 count = vote_p_list[who].length;
+        vcoinInfo[] memory ret = new vcoinInfo[](count);
+        for (uint i = 0; i < count; i++) {
+            ret[i] = votecoin[vote_p_list[msg.sender][i]];
+        }
+        return ret;
+    }
     //投票
     function vote(address coinAddress,bool bStatus) public returns(bool) 
     {
